8a6e06cc79c872e0231e8fd3bbc329d1b71e2e7a
ashvin14 <yashkhrnr@gmail.com>
2019-01-22 23:52:10 +0530

    session issue solved

--- middleware/checkLogin.js
+++ middleware/checkLogin.js
@@ -1,16 +1,9 @@
 exports.check = function(req, res, next) {
-	if (req.session && req.session.user) {
-		//Check if the user is logged in or not
-		next();
-	}
-	//else redirect here
-	else
-		res.status(401).json("Unauthorized access");
-};
+  if (req.session && req.session.user) {
+    //Check if the user is logged in or not
 
-exports.checkLoginType = function(req, res, next) {
-	if (req.body.roles === 'doctor' || req.body.roles === 'patient' && req.body.type === 'unity')
-		next();
-	else
-		res.status(403).json("Forbidden access");
+    next();
+  }
+  //else redirect here
+  else res.status(401).json("Unauthorized access");
 };
