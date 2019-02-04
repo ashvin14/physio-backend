934b0fc526a29bdf7fb0e5dcfb21901c0e6cb470
ashvin14 <yashkhrnr@gmail.com>
2019-02-04 20:02:16 +0530

    changed user seeds

---
+++ db/seeds/05_users.js
@@ -0,0 +1,21 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("users")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("users").insert([
+        {
+          //1
+          username: "nic",
+          password: "123",
+          fullname: "Nick Fury",
+          age: 58,
+          mobile: 7418529632,
+          email: "nic@gmail.com",
+          gender: "Male",
+          roles: "doctor",
+        },
+      ]);
+    });
+};
