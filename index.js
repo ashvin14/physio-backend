9fc61d6bdd6534b54c6747793f7ea07bdf1bd646
ashvin14 <yashkhrnr@gmail.com>
2019-02-04 19:06:22 +0530

    added middleware

--- index.js
+++ index.js
@@ -63,6 +63,10 @@ app.use(
   }),
 );
 
+app.use("/", (req, res) => {
+  res.send("Connection Established!");
+});
+
 fs.readdirSync("./controllers").forEach(function(file) {
   if (file.indexOf(".js")) {
     const route = require("./controllers/" + file);
